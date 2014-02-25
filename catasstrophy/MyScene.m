3dfc89d6f657a0a72eebe454fb4dfe612305e7d7
CSB313CignaFL13 <yuh214@lehigh.edu>
2014-02-25 18:07:56 -0500

    shooting projectiles, with cat movement

--- catasstrophy/MyScene.m
+++ catasstrophy/MyScene.m
@@ -3,11 +3,13 @@
 //  tutorial
 //
 //  Created by CSB313CignaFL13 on 2/11/14.
-//  Copyright (c) 2014 lamesauce. All rights reserved.
+//  Copyright (c) 2014 NESTGaming. All rights reserved.
 //
 
 #import "MyScene.h"
 #import "math.h"
+
+
 @interface MyScene()
 @property (nonatomic) SKSpriteNode * player;
 @property (nonatomic) SKSpriteNode * cat;
@@ -19,7 +21,32 @@
 
 @implementation MyScene
 
--(id)initWithSize:(CGSize)size {    
+static const uint32_t projectileCategory     =  0x1 << 0;
+static const uint32_t monsterCategory        =  0x1 << 1;
+
+static inline CGPoint rwAdd(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint rwSub(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint rwMult(CGPoint a, float b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline float rwLength(CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+// Makes a vector have a length of 1
+static inline CGPoint rwNormalize(CGPoint a) {
+    float length = rwLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
+
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         NSLog(@"Size: %@", NSStringFromCGSize(size));
         
@@ -29,6 +56,8 @@
         self.player.position=CGPointMake(245,280);
         self.cat=[SKSpriteNode spriteNodeWithImageNamed:@"player"];
         self.cat.position=CGPointMake(245,140);
+        self.physicsWorld.gravity = CGVectorMake(0,0);
+        self.physicsWorld.contactDelegate = self;
         self.currentWall = 0;
         [self addChild:self.player];
         [self addChild:self.cat];
@@ -80,7 +109,7 @@
     //int actualDuration = (arc4random() % rangeDuration) + minDuration;
     
     //Create the actions
-    SKAction * actionMove = [SKAction moveTo:CGPointMake(actualX, actualY) duration:5];
+    SKAction * actionMove = [SKAction moveTo:CGPointMake(actualX, actualY) duration:2];
     //SKAction * actionMoveDone = [SKAction actionMov];
     
     [self.cat runAction:[SKAction sequence:@[actionMove]]];
@@ -119,15 +148,57 @@
     //SKAction * actionMoveDone = [SKAction removeFromParent];
     //[monster runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
     
+    
 }
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    // Choose one of the touches to work with
+    UITouch * touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    
+    // Set up initial location of projectile
+    SKSpriteNode * projectile = [SKSpriteNode spriteNodeWithImageNamed:@"projectile"];
+    projectile.position = self.player.position;
+    
+    // Determine offset of location to projectile
+    CGPoint offset = rwSub(location, projectile.position);
+    
+    // Bail out if you are shooting up
+    if (offset.y >= 0) return;
+    
+    // OK to add now - we've double checked position
+    [self addChild:projectile];
+    
+    // Get the direction of where to shoot
+    CGPoint direction = rwNormalize(offset);
+    
+    // Make it shoot far enough to be guaranteed off screen
+    CGPoint shootAmount = rwMult(direction, 1000);
+    
+    // Add the shoot amount to the current position
+    CGPoint realDest = rwAdd(shootAmount, projectile.position);
+    
+    // Create the actions
+    float velocity = 480.0/1.0;
+    float realMoveDuration = self.size.width / velocity;
+    SKAction * actionMove = [SKAction moveTo:realDest duration:realMoveDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+    [projectile runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+    
+}
+
 
 - (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
     
     self.lastSpawnTimeInterval += timeSinceLast;
     self.totalTimeInterval += timeSinceLast;
+    
+    // For evey two seconds add another item/cat
     if (self.lastSpawnTimeInterval > 2) {
         self.lastSpawnTimeInterval = 0;
         //[self addItem];
+        [self updateCat];
     }
 }
 
